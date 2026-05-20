6c947f0424fb048dfdc52d1ba01fe4719a4253ed
Haruhiko Otaka <hakkootaka@gmail.com>
2026-05-20 16:34:18 +0900

    コメント

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import terrabalImg2 from './assets/terrabal2.jpg'
 import chuoImg from './assets/chuo.png'
 import linemoImg from './assets/linemo.png'
 import seikyou from './assets/seikyou.png'
-import conntakuto from './assets/konntakuto.png'
+import konntakuto from './assets/konntakuto.png'
 import ouesu from './assets/ouesu.png'
 import onetimeforever from './assets/1time4ever.png'
 
@@ -159,13 +159,14 @@ export default function App() {
         <div className="hero__bear">🐻</div>
       </section>
 
+
       {/* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-          広告：生協
+          広告：テラバル自動車学校
       ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */}
       <div className="ad-banner">
         <span className="ad-banner__label">広告</span>
-        <a href="https://coop.kyushu-bauc.or.jp/kumamoto-u/index.html" target="_blank" rel="noopener noreferrer" className="ad-banner__link">
-          <img src={seikyou} alt="生協" className="ad-banner__img" />
+        <a href="https://terrabal.co.jp" target="_blank" rel="noopener noreferrer" className="ad-banner__link">
+          <img src={terrabalImg} alt="テラバル自動車学校" className="ad-banner__img" />
         </a>
       </div>
 
@@ -215,42 +216,42 @@ export default function App() {
       </section>
 
       {/* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-          広告：テラバル自動車学校
+          広告：生協
       ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */}
       <div className="ad-banner">
         <span className="ad-banner__label">広告</span>
-        <a href="https://terrabal.co.jp" target="_blank" rel="noopener noreferrer" className="ad-banner__link">
-          <img src={terrabalImg} alt="テラバル自動車学校" className="ad-banner__img" />
+        <a href="https://coop.kyushu-bauc.or.jp/kumamoto-u/index.html" target="_blank" rel="noopener noreferrer" className="ad-banner__link">
+          <img src={seikyou} alt="生協" className="ad-banner__img" />
         </a>
       </div>
 
       {/* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-          広告：テラバル自動車学校
+          広告：コンタクトアイシティ
       ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */}
       <div className="ad-banner">
         <span className="ad-banner__label">広告</span>
         <a href="https://terrabal.co.jp" target="_blank" rel="noopener noreferrer" className="ad-banner__link">
-          <img src={terrabalImg} alt="テラバル自動車学校" className="ad-banner__img" />
+          <img src={konntakuto} alt="コンタクトアイシティ" className="ad-banner__img" />
         </a>
       </div>
 
       {/* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-          広告：テラバル自動車学校
+          広告：オーエス
       ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */}
       <div className="ad-banner">
         <span className="ad-banner__label">広告</span>
         <a href="https://terrabal.co.jp" target="_blank" rel="noopener noreferrer" className="ad-banner__link">
-          <img src={terrabalImg} alt="テラバル自動車学校" className="ad-banner__img" />
+          <img src={ouesu} alt="オーエス" className="ad-banner__img" />
         </a>
       </div>
 
       {/* ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-          広告：テラバル自動車学校
+          広告：ワンタイムフォーエバー
       ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ */}
       <div className="ad-banner">
         <span className="ad-banner__label">広告</span>
         <a href="https://terrabal.co.jp" target="_blank" rel="noopener noreferrer" className="ad-banner__link">
-          <img src={terrabalImg} alt="テラバル自動車学校" className="ad-banner__img" />
+          <img src={onetimeforever} alt="1TIME 4EVER" className="ad-banner__img" />
         </a>
       </div>
 
