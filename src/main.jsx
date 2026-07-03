ed751649516e9f5fb23701f5cb4a475fe83b0f00
Haruhiko Otaka <hakkootaka@gmail.com>
2026-07-03 14:21:54 +0900

    変更

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/pamphlet">
       <App />
     </BrowserRouter>
   </StrictMode>,
