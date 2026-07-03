4958192844a0ac643852fd60fa1d4b6c6b88e4c7
Haruhiko Otaka <hakkootaka@gmail.com>
2026-07-03 17:33:35 +0900

    変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { Routes, Route } from 'react-router-dom'
+import ScrollToTop from './ScrollToTop'
 import HomePage from './pages/HomePage'
 import TentInfoPage from './pages/TentInfoPage'
 
 export default function App() {
   return (
+    <>
+    <ScrollToTop />
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/tent-info" element={<TentInfoPage />} />
     </Routes>
+    </>
+
   )
 }
